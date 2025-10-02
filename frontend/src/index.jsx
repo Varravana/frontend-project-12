aa3a9734b1f545ba43a948c48eb476927708499b
Анастасия Кор <idealizm@mail.ru>
2025-10-02 10:54:49 +0300

    1 step

---
+++ frontend/src/index.jsx
@@ -0,0 +1,6 @@
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import 'reactjs-popup/dist/index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
