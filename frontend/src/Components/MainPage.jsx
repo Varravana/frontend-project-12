7a93bc4a28c5e152fcd559d6aae8fe22501bf9e8
Анастасия Кор <idealizm@mail.ru>
2025-10-07 16:39:32 +0300

    3 step

--- frontend/src/Components/MainPage.jsx
+++ frontend/src/Components/MainPage.jsx
@@ -1,13 +1,42 @@
+import { Button, Form, Container, Row, Col, Image, Card, FloatingLabel } from 'react-bootstrap'
+import { useEffect, useRef, useState } from 'react'
+
 const MainPage = () => {
+
+    const [curentChanal, setCurentChanal] = useState('111')
     const clearLS = () => {
         localStorage.clear()
-        console.log(localStorage)
     }
     return (
         <>
-        <div>Main page</div>
-        <button onClick={()=> clearLS()}>очистить локалсторадж</button>
-            </>
+            <Container className='rounded shadow'>
+                <Row className='bg-white'>
+                    <Col className='bg-light'>
+                        <div>
+                            <b>Каналы</b>
+                            <Button className='primary'>+</Button>
+                        </div>
+                        <ul id='chanals-box'>
+                            <li>
+                                <Button>первый канал</Button>
+                            </li>
+                            <li>
+                                <Button onClick={() => clearLS()}>очистить локалсторадж</Button>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <div className='bg-ligth'>
+                            <span className='text-bold'>{curentChanal} </span>
+                            <span>0 сообщений </span>
+                        </div>
+                        <div id='messages-box' className='px-5'></div>
+                        <div id='input-message' className='px-5 py-3 mt-auto'></div>
+
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
