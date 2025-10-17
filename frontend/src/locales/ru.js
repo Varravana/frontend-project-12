0cdcc03027ba3fbb4ca2d8c26953dac9fb15c199
Анастасия Кор <idealizm@mail.ru>
2025-10-17 14:33:54 +0300

    8 step

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,54 @@
+export default {
+    translation: {
+        channelsButtons: {
+            delete: 'Удалить',
+            rename: 'Переименовать',
+        },
+        exitButton: {
+            exit: 'Выйти'
+        },
+        inputMessageForm: {
+            placeholder: 'Введите сообщение...'
+        },
+        loginPage: {
+            h1: 'Войти',
+            labelName: 'Ваш ник',
+            labelPassword: 'Пароль',
+            error: 'Неверные пароль или логин',
+            submitButton: 'Войти',
+            noAkk: 'Нет аккаунта?',
+            registration: 'Регистрация'
+        },
+        mainPage: {
+            channelsHead: 'Каналы',
+            messageCount: 'сообщений'
+        },
+        notFoundPage: {
+            h3: 'Страница не найдена',
+            span: 'Но вы можете перейти ',
+            href: 'на главную страницу'
+        },
+        SignupPage: {
+            yup: {
+                required: 'Обязательное поле',
+                min3: 'Минимум 3 символа',
+                max20: 'Максимум 20 символов',
+                min6: 'Минимум 6 символов',
+                passwordMatch: 'Пароли должны совпадать'
+            },
+            serverError: {
+                userExists: 'Такой пользователь уже существует',
+                errorRegistration: 'Произошла ошибка при регистрации'
+            },
+            form: {
+                h1: 'Регистрация',
+                labelName: 'Имя пользователя',
+                labelPassword: 'Пароль',
+                labelPasswordConfirm: 'Подтвердите пароль',
+                submitButton: 'Зарегистрироваться'
+            }
+        }
+
+
+    },
+}
