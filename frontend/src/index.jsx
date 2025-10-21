ee0877be7a0e520e381abb80a278c7bf2cdafc96
Анастасия Кор <idealizm@mail.ru>
2025-10-21 13:41:34 +0300

    11 step

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,12 +5,26 @@ import store from './slices/index.js'
 import React from 'react'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18next.js'
+import { ErrorBoundary } from '@rollbar/react'
+
+
+const rollbarConfig = {
+    accessToken: 'ebe2a921609543ff8160a26a5b678576',
+    environment: 'testenv',
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-            <App />
-        </I18nextProvider>
+    <Provider store={store} config={rollbarConfig}>
+        <ErrorBoundary>
+
+
+   
+                <I18nextProvider i18n={i18n}>
+                    <App />
+                </I18nextProvider>
+
+
+        </ErrorBoundary>
     </Provider>
 )
