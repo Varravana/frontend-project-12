7d57db1f37805c6445116ad5b83ac798b4787dca
Анастасия Кор <idealizm@mail.ru>
2025-10-28 14:24:04 +0300

    28.10.5

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -10,7 +10,6 @@ import { useEffect, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import filter from 'leo-profanity'
 
-
 const ChatRoute = ({ children }) => {
   const userLoginToken = localStorage.getItem('token')
 
@@ -20,38 +19,40 @@ const ChatRoute = ({ children }) => {
 }
 
 const App = () => {
-const loginToken = useSelector((state) => state.login.token) 
-const [exitButtonOn, setexitButtonOn]= useState(false)
-filter.add(filter.getDictionary('ru'))
+  const loginToken = useSelector((state) => state.login.token)
+  const [exitButtonOn, setexitButtonOn] = useState(false)
+  filter.add(filter.getDictionary('ru'))
 
-useEffect(()=> {
-console.log(loginToken)
-localStorage.getItem('token') ? setexitButtonOn(true) : setexitButtonOn(false)
-}, [loginToken])
+  useEffect(() => {
+    console.log(loginToken)
+    localStorage.getItem('token') ? setexitButtonOn(true) : setexitButtonOn(false)
+  }, [loginToken])
 
   return (
-    <div className='d-flex flex-column h-100 '>
-      <Navbar bg="white" className='shadow-sm' expand="lg">
+    <div className='d-flex flex-column h-100'>
+      <Navbar bg='white' className='shadow-sm' expand='lg'>
         <Container>
           <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-          {exitButtonOn &&<ExitButton />}
+          {exitButtonOn && <ExitButton />}
         </Container>
       </Navbar>
-
 
       <BrowserRouter>
         <Routes>
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignupPage />} />
-          <Route path="/" element={(
-            <ChatRoute>
-              <MainPage />
-            </ChatRoute>
-          )} />
+          <Route
+            path="/"
+            element={(
+              <ChatRoute>
+                <MainPage />
+              </ChatRoute>
+            )}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-<ToastContainer />
+      <ToastContainer />
     </div>
   )
 }
